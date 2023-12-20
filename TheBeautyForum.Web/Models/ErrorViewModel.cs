b09a4c2209e9318b0080b2802a8e791d9007c0f8
Симона Палиева <simonapalieva05@pmgkk.com>
2023-12-20 08:24:46 +0200

    Add project files.

---
+++ TheBeautyForum.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheBeautyForum.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
